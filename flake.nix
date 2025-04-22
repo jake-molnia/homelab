5689ab0e5cf8708c8082faf19f84e33551120ef3
Jacob Molnia <jrmolnia@wpi.edu>
2025-04-22 00:21:40 -0400

    basic deployment

--- flake.nix
+++ flake.nix
@@ -1,17 +1,41 @@
 {
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-24.11";
+    colmena.url = "github:zhaofengli/colmena";
+    colmena.inputs.nixpkgs.follows = "nixpkgs";
   };
-  outputs = { self, nixpkgs }@inputs: {
-      colmena = {
-        meta = {
-          nixpkgs = import nixpkgs { system = "x86_64-linux"; };
-          specialArgs = { inherit nixpkgs; };
-        };
-        "my-nixos" = { name, nodes, ... }: {
-          deployment.targetHost = "192.168.5.42";
-          deployment.targetUser = "root";
-        };
+
+  outputs = { self, nixpkgs, colmena }@inputs: {
+    # Standard NixOS configurations for each host
+    nixosConfigurations = {
+      electivire = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        modules = [ ./hosts/electivire/configuration.nix ];
+      };
+      yungoos = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        modules = [ ./hosts/yungoos/configuration.nix ];
       };
     };
-  }
+
+    colmena = {
+      meta = {
+        nixpkgs = import nixpkgs { system = "x86_64-linux"; };
+        specialArgs = { inherit nixpkgs; };
+      };
+
+      "electivire" = { name, nodes, ... }: {
+        deployment.targetHost = "10.10.10.195";
+        deployment.targetUser = "root";
+        deployment.buildOnTarget = true;
+        imports = [ ./hosts/electivire/configuration.nix ];
+      };
+
+      #"yungoos" = { name, nodes, ... }: {
+      #  deployment.targetHost = "192.168.5.42";
+      #  deployment.targetUser = "root";
+      #  imports = [ ./hosts/yungoos/configuration.nix ];
+      #};
+    };
+  };
+}
